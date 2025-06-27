fe744f520c6c0a55b3e3742cf04fb498cde16f1f
juliamalta <juliamalta12@gmail.com>
2025-06-27 20:05:57 -0300

    feat:list

--- src/app/WishList/page.tsx
+++ src/app/WishList/page.tsx
@@ -4,28 +4,7 @@ import List from '@/components/sections/List/List'
 export default function WishList() {
     return (
         <>
-            <List
-                cards={[
-                    {
-                        id: 1,
-                        title: 'Dune: Part Two',
-                        vote: '8.8',
-                        year: '(2024)',
-                    },
-                    {
-                        id: 1,
-                        title: 'Dune: Part Two',
-                        vote: '8.8',
-                        year: '(2024)',
-                    },
-                    {
-                        id: 1,
-                        title: 'Dune: Part Two',
-                        vote: '8.8',
-                        year: '(2024)',
-                    },
-                ]}
-            />
+            <List />
         </>
     )
 }
